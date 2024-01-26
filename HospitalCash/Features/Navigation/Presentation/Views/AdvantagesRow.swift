b2f136600fc56c24d5d7df37c064b7bf1841f62f
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-26 18:33:31 +0100

    Add new AdvantageRow

---
+++ HospitalCash/Features/Navigation/Presentation/Views/AdvantagesRow.swift
@@ -0,0 +1,18 @@
+//
+//  AdvantagesRow.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 26.01.24.
+//
+
+import SwiftUI
+
+struct AdvantagesRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdvantagesRow()
+}
