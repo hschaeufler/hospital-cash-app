0929eb129f5ed31756687296e120da7bf0ed1c68
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-26 16:08:13 +0100

    Add Localizable

--- HospitalCash/Features/Navigation/Presentation/Pages/HomePage.swift
+++ HospitalCash/Features/Navigation/Presentation/Pages/HomePage.swift
@@ -9,39 +9,50 @@ import SwiftUI
 
 struct HomePage: View {
     @ScaledMetric var spacing = 20
+    @ScaledMetric var maxHeightButtonGroup = 100
     
     var body: some View {
         VStack(spacing: spacing) {
-            Image(systemName: "cross.case.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(.tint)
-                .frame(maxWidth: 75)
-            Text("Hospital Cash")
-                .font(.largeTitle)
-                .fontDesign(.serif)
-            Divider()
-                .frame(maxWidth: 150)
-            Text("Unser neues Krankentagegeld auf Blockchain-Basis. Jetzt abschließen und von besonders günstigen Preisen profitieren.")
-                .font(.title2)
-                .foregroundColor(Color.gray)
-                .multilineTextAlignment(.center)
-            Divider()
-                .frame(maxWidth: 150)
-            Text("Deine Vorteile")
-                .font(.title2)
-                .bold()
-            Spacer()
-            Button {} label: {
-                Text("Jetzt Beitrag berechnen")
-                    .font(.title3)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 10)
+            VStack {
+                Image(systemName: "cross.case.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.tint)
+                    .frame(maxWidth: 75)
+                Text("Hospital Cash")
+                    .font(.largeTitle)
+                    .fontDesign(.serif)
+                Divider()
+                    .frame(maxWidth: 150)
+                Text("Unser neues Krankentagegeld auf Blockchain-Basis. Jetzt abschließen und von besonders günstigen Preisen profitieren.")
+                    .font(.title2)
+                    .foregroundColor(Color.gray)
+                    .multilineTextAlignment(.center)
+                Divider()
+                    .frame(maxWidth: 150)
+                Text("Deine Vorteile")
+                    .font(.title2)
+                    .bold()
             }
-            .buttonStyle(.bordered)
-            .tint(.blue)
+            .padding()
+            Spacer()
+            ZStack {
+                Rectangle()
+                    .fill(Color(.systemGray5))
+                    .ignoresSafeArea()
+                    .frame(maxHeight: maxHeightButtonGroup)
+                
+                Button {} label: {
+                    Text("Jetzt Beitrag berechnen")
+                        .font(.title3)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 10)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
