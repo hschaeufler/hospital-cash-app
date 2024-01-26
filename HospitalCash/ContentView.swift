233421add2739038e0af93ba01d485edb9ba7205
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-26 05:13:41 +0100

    Initial Commit

---
+++ HospitalCash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 26.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HospitalCash/HospitalCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalCashApp.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 26.01.24.
+//
+
+import SwiftUI
+
+@main
+struct HospitalCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
