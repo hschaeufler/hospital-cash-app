7b80afc19ad9153a7d7ae0fb052daff32cb7af97
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-26 13:01:11 +0100

    Add First Page

--- HospitalCash/ContentView.swift
+++ HospitalCash/ContentView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "cross.case.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .frame(maxWidth: 75)
+            Text("Hospital Cash")
+                .font(.largeTitle)
+                .fontDesign(.serif)
+            Divider()
+            Text("Unser neues Krankentagegeld auf Blockchain-Basis. Jetzt abschließen und von besonders günstigen Preisen profitieren.")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            Divider()
+            Text("Deine Vorteile")
+                .font(.title2)
+                .bold()
+            Spacer()
+            Button {} label: {
+                Text("Jetzt Beitrag berechnen")
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+            }
+            .buttonStyle(.bordered)
+            .tint(.blue)
         }
         .padding()
     }
