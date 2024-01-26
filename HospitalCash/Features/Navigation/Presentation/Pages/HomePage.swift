46d1ef62c64723e7e692982a183976e1e59971a6
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-26 13:20:01 +0100

    Refactor HimePage

---
+++ HospitalCash/Features/Navigation/Presentation/Pages/HomePage.swift
@@ -0,0 +1,45 @@
+//
+//  HomePage.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 26.01.24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "cross.case.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.tint)
+                .frame(maxWidth: 75)
+            Text("Hospital Cash")
+                .font(.largeTitle)
+                .fontDesign(.serif)
+            Divider()
+            Text("Unser neues Krankentagegeld auf Blockchain-Basis. Jetzt abschließen und von besonders günstigen Preisen profitieren.")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            Divider()
+            Text("Deine Vorteile")
+                .font(.title2)
+                .bold()
+            Spacer()
+            Button {} label: {
+                Text("Jetzt Beitrag berechnen")
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+            }
+            .buttonStyle(.bordered)
+            .tint(.blue)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomePage()
+}
