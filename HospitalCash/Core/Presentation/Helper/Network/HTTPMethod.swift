403b81d3def54e828f431799099236f69f9075d8
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-01-31 21:57:01 +0100

    Add network config

---
+++ HospitalCash/Core/Presentation/Helper/Network/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 30.01.23.
+//  Source: https://github.com/hschaeufler/he4lth-app
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET = "GET"
+    case POST = "POST"
+}
---
+++ HospitalCash/Core/Presentation/Helper/Network/RESTClientError.swift
@@ -0,0 +1,30 @@
+//
+//  RESTClientError.swift
+//  HospitalCash
+//
+//  Created by Holger Schäufler on 29.01.23.
+//  Source: https://github.com/hschaeufler/he4lth-app
+
+import Foundation
+
+enum RESTClientError: Error {
+    case invalidURL
+    case serverError(statusCode: Int)
+    case invalidResponse
+    case unauthorized
+}
+
+extension RESTClientError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Ungültige URL"
+        case .serverError(let statusCode):
+            return "Fehler bei der HTTP-Anfrage: Statuscode \(statusCode)"
+        case .invalidResponse:
+            return "Fehler beim Lesen der Response"
+        case .unauthorized:
+            return "Unauthorized"
+        }
+    }
+}
