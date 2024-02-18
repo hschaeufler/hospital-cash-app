a5968e4293708994ab3d76735fc742a36fd4a3c3
hschaeufler <9865991+hschaeufler@users.noreply.github.com>
2024-02-18 21:17:17 +0100

    Add Underwirte Contract Request Model

--- HospitalCash/Features/Premium/Data/Models/UnderwriteContractRequestModel.swift
+++ HospitalCash/Features/Premium/Data/Models/UnderwriteContractRequestModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import web3
+import BigInt
+
+struct UnderwriteContractRequestModel: ABIFunction {
+    public static let name = "applyForInsurace"
+    public let gasPrice: BigUInt? = nil
+    public let gasLimit: BigUInt? = nil
+    public let contract: EthereumAddress
+    public let from: EthereumAddress?=nil
+    
+    public let value: BigUInt
+    let contractApplication: ContractApplicationModel
+    
+    
+    init(_ contract: EthereumAddress,
+         from: EthereumAddress? = nil,
+         value: BigUInt,
+         contractApplication: ContractApplicationModel
+    ) {
+        self.contract = contract
+        self.from = from
+        self.value = value
+        self.contractApplication = contractApplication
+    }
+    
+    public func encode(to encoder: web3.ABIFunctionEncoder) throws {
+        try encoder.encode(value)
+        try encoder.encode(contractApplication)
+    }
+}
